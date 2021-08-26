722fbfe77b5ea69c6bfead45c0cfa93853c9492d
Prashant <53570660+prashant3110@users.noreply.github.com>
2021-08-26 19:57:22 +0530

    Update Find the maximum and minimum element in an array.cpp

--- Array/Find the maximum and minimum element in an array.cpp
+++ Array/Find the maximum and minimum element in an array.cpp
@@ -1,1 +1,29 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter n:";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter elements :";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    
+    int mx=INT_MIN;
+    int mn=INT_MAX;
+    
+    for(int i=0;i<n;i++)
+    {
+        mx=max(mx,arr[i]);
+        mn=min(mn,arr[i]);
+    }
+    
+    cout<<"Maximum element : "<<mx<<endl;
+    cout<<"Minimum element : "<<mn<<endl;
+    
+    return 0;
+    
+}
